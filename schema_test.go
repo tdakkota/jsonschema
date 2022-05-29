d3cdad5b26c47fc260fb1324b9f265da2085870d
tdakkota <tanc13@yandex.ru>
2022-05-29 18:18:10 +0300

    test: add geojson canada.json validator benchmark

--- schema_test.go
+++ schema_test.go
@@ -96,3 +96,27 @@ func TestJSONSchemaSuite(t *testing.T) {
 		})
 	}
 }
+
+var (
+	//go:embed _bench/geojson.json
+	benchSchema []byte
+	//go:embed _bench/canada.json
+	benchData []byte
+)
+
+func BenchmarkSchema_Validate(b *testing.B) {
+	sch, err := Parse(benchSchema)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.SetBytes(int64(len(benchData)))
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if err := sch.Validate(benchData); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
