1309e0c76636a7f8689d8387047479f56e1ac429
tdakkota <tanc13@yandex.ru>
2022-06-05 08:34:03 +0300

    test: add more benchmarks

--- validate_test.go
+++ validate_test.go
@@ -1,30 +1,45 @@
 package jsonschema
 
 import (
-	_ "embed"
+	"embed"
+	"path"
+	"strings"
 	"testing"
 )
 
 var (
-	//go:embed _bench/geojson.json
-	benchSchema []byte
-	//go:embed _bench/canada.json
-	benchData []byte
+	//go:embed _bench
+	bench embed.FS
 )
 
 func BenchmarkValidate(b *testing.B) {
-	sch, err := Parse(benchSchema)
-	if err != nil {
-		b.Fatal(err)
-	}
+	const root = "_bench"
+	for _, e := range mustDir(b, bench, root) {
+		schemaDirPath := path.Join(root, e.Name())
+		b.Run(e.Name(), func(b *testing.B) {
+			benchSchema := mustFile(b, bench, path.Join(schemaDirPath, "schema.json"))
+			sch, err := Parse(benchSchema)
+			if err != nil {
+				b.Skip("Unsupported yet")
+				return
+			}
 
-	b.SetBytes(int64(len(benchData)))
-	b.ReportAllocs()
-	b.ResetTimer()
+			dataDirPath := path.Join(schemaDirPath, "data")
+			for _, f := range mustDir(b, bench, dataDirPath) {
+				benchData := mustFile(b, bench, path.Join(dataDirPath, f.Name()))
+				testName := strings.TrimSuffix(f.Name(), ".json")
+				b.Run(testName, func(b *testing.B) {
+					b.SetBytes(int64(len(benchData)))
+					b.ReportAllocs()
+					b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		if err := sch.Validate(benchData); err != nil {
-			b.Fatal(err)
-		}
+					for i := 0; i < b.N; i++ {
+						if err := sch.Validate(benchData); err != nil {
+							b.Fatal(err)
+						}
+					}
+				})
+			}
+		})
 	}
 }
