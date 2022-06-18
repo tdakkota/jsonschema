41548a9fec586d6431360d71e764bbb4bb781de3
tdakkota <tanc13@yandex.ru>
2022-06-18 22:24:22 +0300

    test: add remotes directory from suite

--- suite_test.go
+++ suite_test.go
@@ -2,14 +2,32 @@ package jsonschema
 
 import (
 	"embed"
+	"io/fs"
+	"net/http"
 	"path"
 	"testing"
+
+	"github.com/go-faster/errors"
 )
 
-//go:embed _testdata
-var testdata embed.FS
+var (
+	//go:embed _testdata
+	testdata embed.FS
+	remotes  = errors.Must(fs.Sub(testdata, path.Join("_testdata", "remotes")))
+)
 
 func TestJSONSchemaSuite(t *testing.T) {
+	h := http.Server{
+		Addr:    "localhost:1234",
+		Handler: http.FileServer(http.FS(remotes)),
+	}
+	defer h.Close()
+
+	go func() {
+		if err := h.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			t.Error(err)
+		}
+	}()
 	runSuite(t, testdata, path.Join("_testdata", "suite"))
 }
 
